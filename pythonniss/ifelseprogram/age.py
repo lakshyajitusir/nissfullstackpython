e7da0bcdfdd66e80385ab45875288b7dd550ff9c
lakshyajitusir <jitu.sahoo54@gmail.com>
2026-02-24 12:17:56 +0530

    elif match

---
+++ pythonniss/ifelseprogram/age.py
@@ -0,0 +1,7 @@
+#Wap take person age form keyboard check eligbal voteing or not 
+print("enter a age ")
+age=int(input())
+#print("eligbal") if age>=18 else print("not eligbal")
+msg="eligbal" if age>=18 else "not eligbal"
+print(msg)
+	
---
+++ pythonniss/ifelseprogram/match1.py
@@ -0,0 +1,13 @@
+"""
+waptake two nos from keyboard enter your choice 1.add 2.sub 3.mult 
+invalid choice  menu driven program"""
+print("enter two nos ")
+no1=int(input())
+no2=int(input())
+print("enter your choice\n1.add\n2.sub\n3.mult")
+ch=int(input())
+match ch:
+	case 1:print("sum=",no1+no2)
+	case 2:print("sub=",no1-no2)
+	case 1:print("mult=",no1*no2)
+	case _:print("invalid choice")
